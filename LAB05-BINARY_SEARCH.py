da454183c2e142ebeee053df2fe0962cdcd72214
taimooransari <ma07595@st.habib.edu.pk>
2022-02-18 21:19:56 +0500

    Q2 Added
    Binary Search Iterative and Modified

--- LAB05-BINARY_SEARCH.py
+++ LAB05-BINARY_SEARCH.py
@@ -23,3 +23,36 @@ def binary_search_iterative(lst,item):
 print(binary_search_iterative(lst, item))
 
 # =====================================================================================
+
+# Q2 - Binary Search Iterative and Modified
+# Extension of Q1, if item doesn't exist add it in list and return the index.
+
+import ast
+lst = input()
+lst = ast.literal_eval(lst)
+item = int(input())
+
+def binary_search_iterative_modified(lst,item):
+    low=0
+    high=len(lst)-1
+    while(high>=low):
+        mid=(low+high)//2
+        if(lst[mid]==item):
+            return mid
+        elif(lst[mid]>item):
+            high = mid-1
+        elif(lst[mid]<item):
+            low = mid+1
+    if(lst[mid]>item):
+        lst.insert(mid,item)
+        return mid
+    elif(lst[mid]<item):
+        lst.insert(mid+1,item)
+        return mid+1
+        
+    
+    
+
+print(binary_search_iterative_modified(lst, item))
+
+# =====================================================================================
