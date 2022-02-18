bd107d9dfd9b076dc6242cfb86d745f0ec590aa3
taimooransari <ma07595@st.habib.edu.pk>
2022-02-18 21:27:28 +0500

    Q4 and Q5 Added
    Binary Search Recursive and Modified.
    Binary Search & Update Records

--- LAB05-BINARY_SEARCH.py
+++ LAB05-BINARY_SEARCH.py
@@ -86,3 +86,78 @@ print(binary_search_recursive(lst, item, low, high))
 
 # =====================================================================================
 
+# Q4 - Binary Search Recursive and Modified
+# Extension of Q3, if item doesn't exist add it in list and return the index.
+
+import ast
+lst = input()
+lst = ast.literal_eval(lst)
+item = int(input())
+low = int(input())
+high = int(input())
+def binary_search_recursive_modified(lst,item,low,high):
+    if(low<=high):
+        mid=(low+high)//2
+        if(lst[mid]==item):
+            return mid
+        elif(lst[mid]>item):
+            high = mid-1
+        elif(lst[mid]<item):
+            low = mid+1
+        if(low>high):
+            if(lst[mid]>=item):
+                lst.insert(mid,item)
+                return mid
+            elif(lst[mid]<item):
+                lst.insert(mid+1,item)
+                return mid+1
+        index = binary_search_recursive_modified(lst,item,low,high)
+        return index
+print(binary_search_recursive_modified(lst, item, low, high))
+
+# =====================================================================================
+
+# Q5 - Binary Search Application
+# Find student record and update data(email/mid1/mid2), don't change ID
+
+import ast
+student_records = input()
+student_records = ast.literal_eval(student_records)
+ID = input()
+record_title = input()
+data = input()
+
+def update_record(student_records,ID,record_title,data):
+    if(record_title=='ID'):
+        return "ID cannot be updated"
+    if(record_title!='Email'):
+        data=int(data)
+    dic = {"Email":1,'Mid1':2,'Mid2':3}
+    index_to_change = dic[record_title]
+    low=0
+    high=len(student_records)-1
+    
+    while(low<=high):
+        mid=(low+high)//2
+        rec = student_records[mid][0]
+        if(rec==ID):
+            temp = list(student_records[mid])
+            temp[index_to_change]=data
+            student_records[mid]=tuple(temp)
+            return student_records
+        elif(rec>ID):
+            high = mid-1
+        elif(rec<ID):
+            low = mid+1
+    return "Record not found"
+        
+    
+    
+    
+
+print(update_record(student_records, ID, record_title, data))
+
+# =====================================================================================
+
+
+
